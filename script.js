e7ef3d356af082c299de255514970406241b53b0
Ranu <88536226+Ranu2211@users.noreply.github.com>
2025-01-17 18:36:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function validEmail(str) {
   //your JS code here.
+	if (!str || typeof str !== 'string') {
+        return false;
+    }
+const emailPattern = /^[a-zA-Z0-9]([-._]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.-]?[a-zA-Z0-9]+)*(\.[a-zA-Z]{2,})+$/;
+    
+    return emailPattern.test(str);
 }
 
 // Do not change the code below.
 const str = prompt("Enter an email address.");
+
 alert(validEmail(str));
